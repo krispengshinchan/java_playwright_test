951c1eb469cabae215b290a546442df5f7cd9b7a
krispengshinchan <krispengshinchan@163.com>
2025-05-08 19:58:03 +0800

    change

--- src/test/java/org/example/TestExample.java
+++ src/test/java/org/example/TestExample.java
@@ -58,12 +58,12 @@ public class TestExample {
         assertTrue((Boolean) page.evaluate("() => window['checkbox'].checked"));
     }
 
-    @Test
-    void shouldSearchWiki() {
-        page.navigate("https://www.wikipedia.org/");
-        page.locator("input[name=\"search\"]").click();
-        page.locator("input[name=\"search\"]").fill("playwright");
-        page.locator("input[name=\"search\"]").press("Enter");
-        assertEquals("https://en.wikipedia.org/wiki/Playwright", page.url());
-    }
+//    @Test
+//    void shouldSearchWiki() {
+//        page.navigate("https://www.wikipedia.org/");
+//        page.locator("input[name=\"search\"]").click();
+//        page.locator("input[name=\"search\"]").fill("playwright");
+//        page.locator("input[name=\"search\"]").press("Enter");
+//        assertEquals("https://en.wikipedia.org/wiki/Playwright", page.url());
+//    }
 }
